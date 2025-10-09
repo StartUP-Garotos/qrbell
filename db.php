e5acc073b2a0392c8c23ed0b7555ee82fc3ab5e4
ArthurLoffi <arthurloffisoares@outlook.com>
2025-10-09 16:37:16 -0300

    Ajuste de PDO

--- db.php
+++ db.php
@@ -5,10 +5,11 @@ $user = "if0_40131980";
 $pass = "EjtZdaxWKyidMXT";
 $db_name = "if0_40131980_qrbell";
 
-try{
-    $conn = new PDO("mysql: host=$host;dbname=$db_name", $user, $pass);
-    $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e){
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db_name", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
+
 ?>
