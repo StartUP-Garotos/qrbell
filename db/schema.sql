5051890fd4911b212d6fe89c7a3b67eb26fbc04c
ArthurLoffi <arthurloffisoares@outlook.com>
2025-10-16 21:27:38 -0300

    Arrumado o db com valores genericos para teste

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,5 @@ CREATE TABLE residencias (
     proprietario VARCHAR(255) NOT NULL,
     telefone VARCHAR(20) NOT NULL
 );
+
+INSERT INTO residencias (numero, proprietario, telefone) VALUES ("101","Arthur","47996792788"), ("102","Vitor","47991075493");
